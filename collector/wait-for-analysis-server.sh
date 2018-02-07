0cd4c2137401cf0f41988b719fa496162822d382
Tim Veil <tjveil@gmail.com>
2018-02-07 09:04:43 -0500

    removing port variable; working on agent

--- collector/wait-for-analysis-server.sh
+++ collector/wait-for-analysis-server.sh
@@ -3,10 +3,8 @@
 set -e
 
 key="$1"
-port="$2"
 
 >&2 echo "key is $key"
->&2 echo "port is $port"
 
 until $(curl --output /dev/null --silent --head --fail http://analysis-server:8080); do
   >&2 echo "Analysis Server is unavailable - sleeping"
@@ -15,6 +13,6 @@ done
 
 >&2 echo "Analysis Server is up - starting collector install"
 
-wget -O - -o /dev/null "http://analysis-server:8080/app/download?t=inst" | bash /dev/stdin -i --sk=$key --s3_url http://analysis-server:8080/service/png --installer_url "http://analysis-server:8080/app/download?t=tgz" --base_url http://analysis-server:8080 --machine_name=collector --listen_on_port=$port
+wget -O - -o /dev/null "http://analysis-server:8080/app/download?t=inst" | bash /dev/stdin -i --sk=$key --s3_url http://analysis-server:8080/service/png --installer_url "http://analysis-server:8080/app/download?t=tgz" --base_url http://analysis-server:8080 --machine_name=collector --listen_on_port=6060
 
 /opt/takipi/bin/takipi-service -nfd -l
