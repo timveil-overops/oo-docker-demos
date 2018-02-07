21f694df7a6d1ad328d60f6c51a8ce20e806e9fd
Tim Veil <tjveil@gmail.com>
2018-02-07 11:19:41 -0500

    adding scala and app name

--- agent-java-jam/wait-for-collector.sh
+++ agent-java-jam/wait-for-collector.sh
@@ -15,6 +15,6 @@ done
 
 wget -O - -o /dev/null "http://analysis-server:8080/app/download?t=inst" | bash /dev/stdin -i --sk=$key --s3_url http://analysis-server:8080/service/png --installer_url "http://analysis-server:8080/app/download?t=tgz" --base_url http://analysis-server:8080 --machine_name=java-agent --daemon_host=collector --daemon_port=6060
 
-java -agentlib:TakipiAgent -jar javajam.jar
+java -agentlib:TakipiAgent -Dtakipi.name=JavaJam -jar javajam.jar
 
 tail -f /dev/null
---
+++ agent-scala-boom/wait-for-collector.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+
+key="$1"
+
+>&2 echo "key is $key"
+
+until $(curl --output /dev/null --silent --head --fail http://analysis-server:8080); do
+  >&2 echo "Analysis Server is unavailable - sleeping"
+  sleep 1
+done
+
+>&2 echo "Analysis Server is up - starting Scala Agent install"
+
+wget -O - -o /dev/null "http://analysis-server:8080/app/download?t=inst" | bash /dev/stdin -i --sk=$key --s3_url http://analysis-server:8080/service/png --installer_url "http://analysis-server:8080/app/download?t=tgz" --base_url http://analysis-server:8080 --machine_name=scala-agent --daemon_host=collector --daemon_port=6060
+
+java -agentlib:TakipiAgent -Dtakipi.name=ScalaBoom -jar scala-boom.jar
+
+tail -f /dev/null
