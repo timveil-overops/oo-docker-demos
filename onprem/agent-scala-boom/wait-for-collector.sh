677a4d71b319600cde6ad8fe0f9a963d162b3332
Tim Veil <tjveil@gmail.com>
2018-04-06 10:05:38 -0400

    adding deployment names

--- onprem/agent-scala-boom/wait-for-collector.sh
+++ onprem/agent-scala-boom/wait-for-collector.sh
@@ -15,6 +15,6 @@ done
 
 wget -O - -o /dev/null "http://analysis-server:8080/app/download?t=inst" | bash /dev/stdin -i --sk=$key --s3_url http://analysis-server:8080/service/png --installer_url "http://analysis-server:8080/app/download?t=tgz" --base_url http://analysis-server:8080 --machine_name=scala-agent --daemon_host=collector --daemon_port=6060
 
-java -agentlib:TakipiAgent -Dtakipi.name=ScalaBoom -jar scala-boom.jar
+java -agentlib:TakipiAgent -Dtakipi.name=ScalaBoom -Dtakipi.deployment.name=v1.0 -jar scala-boom.jar
 
 tail -f /dev/null
