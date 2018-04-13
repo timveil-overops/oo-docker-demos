e8fe1f7a63b89b76844deb7d8165ebdb0134123e
Tim Veil <tjveil@gmail.com>
2018-04-13 14:28:04 -0400

    got elk working sort of

--- onprem/agent-java-jam/wait-for-collector.sh
+++ onprem/agent-java-jam/wait-for-collector.sh
@@ -15,4 +15,8 @@ done
 
 wget -O - -o /dev/null "http://analysis-server:8080/app/download?t=inst" | bash /dev/stdin -i --sk=$key --s3_url http://analysis-server:8080/service/png --installer_url "http://analysis-server:8080/app/download?t=tgz" --base_url http://analysis-server:8080 --machine_name=java-agent --daemon_host=collector --daemon_port=6060
 
-java -agentlib:TakipiAgent -Dtakipi.name=JavaJam -Dtakipi.deployment.name=v1.0 -Dtakipi.debug.logconsole -jar javajam.jar
+/etc/init.d/filebeat start
+
+#java -agentlib:TakipiAgent -Dtakipi.name=JavaJam -Dtakipi.deployment.name=v1.0 -Dtakipi.debug.logconsole -jar javajam.jar
+
+tail -f /dev/null
