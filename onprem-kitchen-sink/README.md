16586e9d9256dd5c7cf9d218b3d87d85b9ad3ef6
Tim Veil <tjveil@gmail.com>
2018-05-11 19:38:34 -0400

    adding kitchen sink

---
+++ onprem-kitchen-sink/README.md
@@ -0,0 +1,31 @@
+# Instructions
+The on-prem demo includes a docker image for the analysis server, remote collector and two agents (one java and one scala).  To begin, you must first update the value for `SECRET_KEY` in `.env` then run the following commands.
+
+Build the images
+```
+docker-compose build --no-cache
+```
+
+Start the images
+```
+docker-compose up
+```
+
+Stop and destroy the images
+```
+docker-compose down
+```
+
+
+Once the server is up, you can access the OverOps UI by going to http://localhost:8080/login.html or http://analysis-server:8080/login.html.  You may need to add the following entry to your hosts file.
+
+```
+127.0.0.1       analysis-server
+```
+
+Username and password are as follows:
+
+Username: `admin@takipi.com`
+Password: `123456`
+
+ELK http://localhost:5601/
---
+++ onprem-kitchen-sink/agent-java/startup.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+set -e
+
+key="$1"
+
+>&2 echo "key is $key"
+
+until $(curl --output /dev/null --silent --head --fail http://analysis-server:8080); do
+  >&2 echo "Analysis Server is unavailable - sleeping"
+  sleep 1
+done
+
+>&2 echo "Analysis Server is up - starting Java Agent install"
+
+wget -O - -o /dev/null "http://analysis-server:8080/app/download?t=inst" | bash /dev/stdin -i --sk=$key --s3_url http://analysis-server:8080/service/png --installer_url "http://analysis-server:8080/app/download?t=tgz" --base_url http://analysis-server:8080 --machine_name=java-agent --daemon_host=collector --daemon_port=6060
+
+/etc/init.d/filebeat start
+
+java -agentlib:TakipiAgent -Dtakipi.name=JavaGenerator -Dtakipi.deployment.name=v1.0 -Dtakipi.debug.logconsole -jar overops-event-generator.jar
+
+#tail --retry -f -n +1 /tmp/takipi-install/overops-event-generator.log
